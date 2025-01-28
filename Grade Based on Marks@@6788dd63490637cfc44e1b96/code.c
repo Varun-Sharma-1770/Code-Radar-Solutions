ba6a263468d0ca42d5aedb47f7331502e9c03009
Varun-Sharma-1770 <varun1770.becse24@chitkara.edu.in>
2025-01-28 18:14:24 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int x;
+    scanf("%d", &x);
+    if(x>=90){
+        printf("A");
+    }
+    else if(80<=x<90){
+        printf("B");
+    }
+    else if(70<=x<80){
+        printf("C");
+    }
+    else if(60<=x<70){
+        print("D");
+    }
+    else{
+        printf("F");
+    }
+    return 0;
+}
