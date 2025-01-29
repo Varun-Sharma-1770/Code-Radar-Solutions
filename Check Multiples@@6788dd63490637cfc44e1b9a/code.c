fbeb0154243a93da6ec81b7f9bc539ef1241589d
Varun-Sharma-1770 <varun1770.becse24@chitkara.edu.in>
2025-01-29 19:18:55 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int x;
+    scanf("%d", &x);
+    int y;
+    sanf("%d", &y);
+    if(x%y==0){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
